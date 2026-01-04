6f36f1d02fbc928b1ca380ab6739b92d3673443e
ToanfKhoa <quachbuitoankhoa@gmail.com>
2026-01-04 16:54:25 +0700

    feat: Loading page

---
+++ Assets/Scripts/LoadingManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class LoadingManager : MonoBehaviour
+{
+    public Slider progressBar;
+    public TextMeshProUGUI progressText;
+
+    void Start()
+    {
+        StartCoroutine(LoadGame());
+    }
+
+    IEnumerator LoadGame()
+    {
+        AsyncOperation load = SceneManager.LoadSceneAsync(1);
+        load.allowSceneActivation = false;
+
+        while (!load.isDone)
+        {
+            float progress = Mathf.Clamp01(load.progress / 0.9f);
+            progressBar.value = progress;
+            progressText.text = Mathf.RoundToInt(progress * 100) + "%";
+
+            // 100%
+            if (load.progress >= 0.9f)
+            {
+                progressBar.value = 1;
+                progressText.text = "100%";
+                yield return new WaitForSeconds(0.3f); 
+                load.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
