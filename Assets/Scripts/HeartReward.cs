c7933ab960b33a7d8c7133020f595a3b6ea877a4
Quach Bui Song Dang <quachbuisongdang@gmail.com>
2026-01-06 15:40:19 +0700

    feat: add update item count

--- Assets/Scripts/HeartReward.cs
+++ Assets/Scripts/HeartReward.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnitonConnect.Core;
+using UnitonConnect.Core.Utils.Debugging;
 
 public class HeartReward : MonoBehaviour
 {
@@ -7,6 +8,37 @@ public class HeartReward : MonoBehaviour
     {
         Debug.Log("Get heart reward");
         string jsonParams = "{\"amountHeart\": 1}"; //dummy
+
+        UnitonConnectSDK.Instance.OnTonTransactionSended += OnTxSuccess;
+        UnitonConnectSDK.Instance.OnTonTransactionSendFailed += OnTxFailed;
+
         UnitonConnectSDK.Instance.SendSmartContractTransaction("heart_reward", jsonParams);
-    }    
+    }
+    private void OnTxFailed(string errorMsg)
+    {
+        UnitonConnectLogger.LogError("Heart reward failed: " + errorMsg);
+
+        Cleanup();
+    }
+
+    private void OnTxSuccess(string transactionHash)
+    {
+        UnitonConnectLogger.Log("Heart reward success, hash: " + transactionHash);
+
+        int heartCount = 2 + PlayerStatsManager.Instance.levelHeart; //lv0: 2, lv1: 3, lv2: 4
+        PlayerStatsManager.Instance.AddHeart(heartCount);
+
+        Cleanup();
+    }
+    private void Cleanup()
+    {
+        //cancel loading pop up;
+
+        if (UnitonConnectSDK.Instance != null)
+        {
+            UnitonConnectSDK.Instance.OnTonTransactionSended -= OnTxSuccess;
+            UnitonConnectSDK.Instance.OnTonTransactionSendFailed -= OnTxFailed;
+        }
+
+    }
 }
